b4c3f855258827da5698985d4bbcaf98f92cb2a4
derekaj16 <dprodigy@byu.edu>
2023-12-14 17:47:01 -0700

    url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   async function getPlanes() {
     try {
-      const response = await axios.get('http://34.217.46.210:8080');
+      const response = await axios.get('http://54.190.158.141:8080');
       return response.data; // Assuming the data is in response.data
     } catch (error) {
       console.log(error);
